fee0aa93701c0fb50e54cece480bbe06b2566f54
abaddonGIT <abaddongit@gmail.com>
2014-12-20 00:18:20 +0400

    and jshint, cache for image and url replace in html file

--- gulpfile.js
+++ gulpfile.js
@@ -1,55 +1,79 @@
 /**
  * Created by abaddon on 18.12.2014.
  */
-var gulp = require('gulp');
+(function (require) {
+    "use strict";
+    var gulp = require('gulp');
 
-var gulpLoadPlugins = require('gulp-load-plugins');
-var replace = require('gulp-html-replace');
-var plugins = gulpLoadPlugins();
-console.log(plugins);
-//Оптимизация сериптов
-gulp.task("js", function () {
-    gulp.src("js/*.js")
-        .pipe(plugins.concat("all.min.js"))
-        .pipe(plugins.uglify())
-        .pipe(gulp.dest("dist/js/"));
-});
-//Оптимизация стилей
-gulp.task("css", function () {
-    gulp.src("css/*.css")
-        .pipe(plugins.concat("style.min.css"))
-        .pipe(plugins.autoprefixer({
-            browsers: ['last 2 versions']
-        }))
-        .pipe(plugins.csso())
-        .pipe(gulp.dest("dist/css/"));
-});
-//Оптимизация изображений
-gulp.task("img", function () {
-    //Создание спрайтов
-    var sprite = gulp.src("img/sp/*.png")
-        .pipe(plugins.spritesmith({
-            imgName: "sp.png",
-            cssName: "sp.less",
-            cssFormat: "less",
-            padding: 5,
-            engineOpts: {
-                "imagemagick": true
-            }
-        }));
-    sprite.img.pipe(gulp.dest('img/'));
-    sprite.css.pipe(gulp.dest('css/'));
-    //Минификация изображений
-    gulp.src(['img/*.jpg', 'img/*.png', 'img/*.gif'])
-        .pipe(plugins.imagemin())
-        .pipe(gulp.dest("dist/img/"));
-});
-//Перенос файлов
-gulp.task("html", function () {
-    gulp.src("*.html")
-        .pipe(replace({
-            css: "css/style.min.css",
-            js: "js/all.min.js"
-        }))
-        .pipe(gulp.dest("dist/"));
-});
+    var gulpLoadPlugins = require('gulp-load-plugins');
+    var plugins = gulpLoadPlugins();
+
+    /*
+     *Оптимизация сериптов
+     */
+    gulp.task("js", function () {
+        gulp.src("js/*.js")
+            .pipe(plugins.jshint(".jshintrc"))
+            .pipe(plugins.jshint.reporter("default"))
+            .pipe(plugins.concat("all.min.js"))
+            .pipe(plugins.uglify())
+            .pipe(gulp.dest("dist/js/"));
+    });
+    /*
+     *Оптимизация стилей
+     */
+    gulp.task("css", function () {
+        gulp.src("css/*.css")
+            .pipe(plugins.concat("style.min.css"))
+            .pipe(plugins.autoprefixer({
+                browsers: ['last 2 versions']
+            }))
+            .pipe(plugins.csso())
+            .pipe(gulp.dest("dist/css/"));
+    });
+    /*
+     *Оптимизация изображений
+     */
+    gulp.task("img", function () {
+        //Создание спрайтов
+        var sprite = gulp.src("img/sp/*.png")
+            .pipe(plugins.spritesmith({
+                imgName: "sp.png",
+                cssName: "sp.less",
+                cssFormat: "less",
+                padding: 5,
+                engineOpts: {
+                    "imagemagick": true
+                }
+            }));
+        sprite.img.pipe(gulp.dest('img/'));
+        sprite.css.pipe(gulp.dest('css/'));
+        //Минификация изображений
+        gulp.src(['img/*.jpg', 'img/*.png', 'img/*.gif'])
+            .pipe(plugins.cache(plugins.imagemin({optimizationLevel: 5, progressive: true, interlaced: true})))
+            .pipe(gulp.dest("dist/img/"));
+    });
+    /*
+     *Замена адресов и перенос файлов
+     */
+    gulp.task("html", function () {
+        gulp.src("*.html")
+            .pipe(plugins.replace(/style.css/g, "style.min.css"))
+            .pipe(plugins.replace(/base.js/g, "base.min.js"))
+            .pipe(gulp.dest("dist/"));
+    });
+    /*
+     * Наблюдатель
+     */
+    gulp.task("watch", function () {
+        gulp.watch("css/*.css", ['css']);
+        gulp.watch("js/*js", ['js']);
+        gulp.watch("img/**/*", ['img']);
+    });
+    /*
+     *Сборка проекта
+     */
+    gulp.task("default", function () {
+        gulp.start("js", "css", "img", "html");
+    });
+}(require));
--- js/base.js
+++ js/base.js
@@ -1,7 +1,7 @@
 /**
  * Created by abaddon on 18.12.2014.
  */
-/*global window, document*/
+/*global window, document, console*/
 (function (w, d) {
     "use strict";
     var App = function () {
