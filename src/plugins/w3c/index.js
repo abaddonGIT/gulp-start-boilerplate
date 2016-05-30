e832c749ceacd496a506da7e3f003a143e782374
abaddonGIT <abaddongit@gmail.com>
2016-05-30 17:52:25 +0300

    work

--- src/plugins/w3c/index.js
+++ src/plugins/w3c/index.js
@@ -7,7 +7,7 @@ var through = require('through2');
 var w3cjs = require('w3cjs');
 var gutil = require('gulp-util');
 var merge = require('object-assign');
-var fs = require("fs");
+//var fs = require("fs");
 
 /**
  * Handles messages.
@@ -19,10 +19,36 @@ var fs = require("fs");
 
 var TAG = "gulp-w3c-validate";
 
-var showError = function (response, file) {
+/**
+ * Create and show console messages view
+ * @param response
+ * @param file
+ */
+var showConsoleMessages = function (response, file) {
     console.log(response);
 };
 
+/**
+ * Create and view html messsages view
+ * @param response
+ */
+var showHtmlMessages = function (response, file) {
+    var content = file.contents.toString(), join = '</body>', validateText = "";
+    var htmlParts = content.split(join);
+    //console.log(htmlParts);
+    if (htmlParts.length > 1) {
+        validateText = "<!------------------VALIDATE---------------->EBA<!------------------VALIDATE---------------->";
+    }
+    htmlParts[0] += validateText;
+
+    return prefixStream(htmlParts.join(join));
+};
+
+/**
+ * Create and return file like stream
+ * @param prefixText
+ * @returns {*}
+ */
 function prefixStream(prefixText) {
     var stream = through();
     stream.write(prefixText);
@@ -35,13 +61,14 @@ var validate = function (params) {
     }
     params = merge(params, {
         type: ['css', 'html'],
-        view: "page",//page
+        view: "html",//console
         showJsPath: ""
     });
 
 
     return through.obj(function (file, enc, callback) {
-        var self = this;
+        var content = file.contents.toString();
+
         if (file.isNull()) {
             callback(null, file);
         }
@@ -50,11 +77,28 @@ var validate = function (params) {
             return callback(new gutil.PluginError('gulp-w3cjs', 'Streaming not supported'));
         }
 
+        if (params.view === "html") {
+            var w3cPart = content.split('<!------------------VALIDATE---------------->');
+            //console.log(w3cPart.length);
+            if (w3cPart.length > 1) {
+                w3cPart.splice(1, 1);
+                console.log(w3cPart);
+                file.contents = prefixStream(w3cPart.join(""));
+            }
+        }
+
         w3cjs.validate({
             input: file.contents,
             output: 'json',
             callback: function (res) {
-                showError(res, file);
+                switch (params.view) {
+                    case "html":
+                        file.contents = showHtmlMessages(res, file);
+                        break;
+                    case "console":
+                        showConsoleMessages(res, file);
+                        break;
+                }
                 //if (params.view === "page") {
                 //    var content = file.contents.toString(), join = '</footer>';
                 //    var code = content.split(join);
