b1d341011121686e9639622088b13b5791e34311
abaddonGIT <abaddongit@gmail.com>
2016-06-06 01:09:35 +0300

    replace mincss to cleancss

--- gulp/tasks/mincss.js
+++ gulp/tasks/mincss.js
@@ -2,13 +2,13 @@
  * Created by abaddon on 27.01.2015.
  */
 var gulp = require('gulp'),
-    mincss = require('gulp-minify-css'),
+    cleanCSS = require('gulp-clean-css'),
     size = require('gulp-filesize'),
     config = require('../config').cssmin;
 
 gulp.task('mincss', ['prefix'], function () {
     return gulp.src(config.src)
-        .pipe(mincss({keepBreaks: true, keepSpecialComments: '*'}))
+        .pipe(cleanCSS({compatibility: 'ie9'}))
         .pipe(gulp.dest(config.dest))
         .pipe(size());
 });
