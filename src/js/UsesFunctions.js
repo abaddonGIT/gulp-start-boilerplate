ec59383e13187ac2b34b91e63a19f58ea53c39ca
abaddonGIT <abaddongit@gmail.com>
2016-07-10 03:24:18 +0300

    add autoHeight function

--- src/js/UsesFunctions.js
+++ src/js/UsesFunctions.js
@@ -29,5 +29,26 @@ export default {
         any: function () {
             return (this.Android() || this.BlackBerry() || this.iOS() || this.Opera() || this.Windows());
         }
+    },
+    /**
+     * Set max height
+     * @param selector
+     * @returns {boolean}
+     */
+    autoHeight: function (selector) {
+        let all = document.querySelectorAll(selector), ln = all.length;
+        if (!ln) return false;
+        let max = 0;
+        for (let i = 0; i < ln; i++) {
+            let item = all[i], height = item.offsetHeight;
+            if (max < height) {
+                max = height;
+            }
+        }
+
+        for (let i = 0; i < ln; i++) {
+            let item = all[i];
+            item.style.cssText += "min-height: " + max + "px;";
+        }
     }
 };
