260580e7af768764d49399abb7bb064a6926dff1
abaddonGIT <abaddongit@gmail.com>
2017-11-15 14:28:46 +0300

    fix production compile

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -1,14 +1,15 @@
 /**
  * Created by abaddon on 27.01.2015.
  */
-var gulp = require('gulp'),
-    config = require('../config').html;
+const gulp = require('gulp'),
+  config = require('../config').html;
 
 gulp.task("html", function () {
-    gulp.src(config.src).pipe(gulp.dest(config.dest));
-    gulp.src("./src/css/**/*.css").pipe(gulp.dest('./dist/css'));
+  gulp.src(config.src).pipe(gulp.dest(config.dest));
+  gulp.src("./src/css/style.css").pipe(gulp.dest('./dist/css/'));
 
-    gulp.src("./src/css/fonts/**").pipe(gulp.dest("./dist/css/fonts"));
-    gulp.src(config.vendorsSrc).pipe(gulp.dest(config.vendorsDest));
-    gulp.src(config.fontsSrc).pipe(gulp.dest(config.fontsDest));
+  gulp.src("./src/css/fonts/**").pipe(gulp.dest("./dist/css/fonts"));
+  gulp.src("./src/js/vendors/badIe.js").pipe(gulp.dest("./dist/js/vendors/"));
+  gulp.src("./src/js/vendors/jquery/jquery.min.js").pipe(gulp.dest("./dist/js/vendors/jquery/"));
+  gulp.src("./src/js/app.compile.js").pipe(gulp.dest("./dist/js/"));
 });
